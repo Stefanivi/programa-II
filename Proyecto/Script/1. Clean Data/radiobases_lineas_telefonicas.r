63620b92ce5af0e95d0b2ea9c9fd2699314167b7
Stefanivi <61399733+Stefanivi@users.noreply.github.com>
2025-10-03 18:45:02 -0600

    Telefonia

--- Proyecto/Script/1. Clean Data/radiobases_lineas_telefonicas.r
+++ Proyecto/Script/1. Clean Data/radiobases_lineas_telefonicas.r
@@ -11,6 +11,41 @@ library(readr)
 graphics.off()
 rm(list=ls())
 cat("\014")
-setwd("/Users/stefanivilleda/Desktop/Programación II/Proyecto/programa-II/Proyecto/output/")
-#Carga de base
-hogares <- read_excel("./input/ENCOVI_hogares.xlsx")
+setwd("/Users/stefanivilleda/Desktop/Programación II/Proyecto/programa-II/Proyecto/")
+#Carga de bases de datos
+?read_excel
+radiobases <- read_excel("./input/Radiobases.xlsx",sheet = "2023")
+telefonia_fija <- read_excel("./input/Telefonía fija.xlsx")
+
+
+#Radiobases
+head(radiobases)
+
+#quitar la primera y segunda fila de NA
+radiobases <- radiobases[-c(1,2),]
+#Renombrar columnas
+colnames(radiobases) <- c("departamento","municipio","radiobases")
+head(radiobases)
+#Eliminar  columnas con NA despues de radiobases
+radiobases <- radiobases %>% select(departamento, municipio, radiobases)
+head(radiobases)
+
+#Sumar la columna de radiobases por departamento 
+radiobases <- radiobases %>%
+  group_by(departamento) %>%
+  summarise(radiobases = sum(as.numeric(radiobases), na.rm = TRUE))
+head(radiobases)
+#Poner solo el nombre total en vez de NA de radiobases en la ultima fila de la tabla radiobases
+radiobases[nrow(radiobases),1] <- "Total"
+
+#Lineas fijas telefonicas
+head(telefonia_fija)
+
+# Hacer todas las variables del mismo
+
+
+
+
+#Hacerlo como tabla, arriba están los años y para abajo los departamentos
+
+
